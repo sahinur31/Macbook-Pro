b9ed097e5d38025dae70ae03258d20ebcaebbd1f
sahinur31 <sahinur4841@gmail.com>
2021-08-21 10:34:34 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+document.getElementById('memory-eight').addEventListener('click',function(){
+     const memoryEightButton = document.getElementById('memory-cost');
+     memoryEightButton.innerText = 0;
+
+});
+document.getElementById('memory-sixteen').addEventListener('click',function(){
+     const memorySixteenButton = document.getElementById('memory-cost');
+     memorySixteenButton.innerText = 180;
+
+});
+document.getElementById('memory-sixteen').addEventListener('click',function(){
+     const memorySixteenButton = document.getElementById('memory-cost');
+     memorySixteenButton.innerText = 180;
+
+});
+document.getElementById('storage-256').addEventListener('click',function(){
+     const storageButton = document.getElementById('storage-cost');
+     storageButton.innerText = 0;
+
+});
+document.getElementById('storage-512').addEventListener('click',function(){
+     const storageButton = document.getElementById('storage-cost');
+     storageButton.innerText = 100;
+
+});
+document.getElementById('storage-1tb').addEventListener('click',function(){
+     const storageButton = document.getElementById('storage-cost');
+     storageButton.innerText = 180;
+
+});
