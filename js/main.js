cfd385edbb79bbf328904e39251f27f7455adfcc
sahinur31 <sahinur4841@gmail.com>
2021-08-21 20:36:11 +0600

    final commit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,83 @@
-document.getElementById('memory-eight').addEventListener('click',function(){
-     const memoryEightButton = document.getElementById('memory-cost');
-     memoryEightButton.innerText = 0;
+// variable decleration area
+const memoryEight = document.getElementById('memory-eight');
+const memorySixteen = document.getElementById('memory-sixteen');
+const storage256 = document.getElementById('storage-256');
+const storage512 = document.getElementById('storage-512');
+const storage1tb = document.getElementById('storage-1tb');
+const deliveryFree = document.getElementById('delivery-free');
+const deliveryTwo = document.getElementById('delivery-two');
 
+const memoryCost = document.getElementById('memory-cost');
+const storageCost = document.getElementById('storage-cost');
+const deliveryCost = document.getElementById('delivery-cost');
+
+
+
+// update total function
+function updateTotal(){
+    const memoryCharge = parseFloat(memoryCost.innerText);
+    const storageCharge = parseFloat(storageCost.innerText);
+    const deliveryCharge = parseFloat(deliveryCost.innerText);
+    const totalPrice = document.getElementById('total-price');
+    const grandTotal = 1299 + memoryCharge +storageCharge + deliveryCharge;
+    totalPrice.innerHTML = grandTotal;
+    const totalPriceFinal = document.getElementById('total-price-final');
+    totalPriceFinal.innerHTML = grandTotal;
+
+    
+}
+
+// cost update
+memoryEight.addEventListener('click',function(){
+    memoryCost.innerText ="0";
+    updateTotal();
+ });
+memorySixteen.addEventListener('click',function(){ 
+    memoryCost.innerText ="180";
+    updateTotal();
 });
-document.getElementById('memory-sixteen').addEventListener('click',function(){
-     const memorySixteenButton = document.getElementById('memory-cost');
-     memorySixteenButton.innerText = 180;
-
+storage256.addEventListener('click',function(){
+    storageCost.innerText ="0";
+    updateTotal();
+ });
+storage512.addEventListener('click',function(){
+    storageCost.innerText ="100";
+    updateTotal();
+ });
+storage1tb.addEventListener('click',function(){ 
+    storageCost.innerText ="180";
+    updateTotal();
 });
-document.getElementById('memory-sixteen').addEventListener('click',function(){
-     const memorySixteenButton = document.getElementById('memory-cost');
-     memorySixteenButton.innerText = 180;
+deliveryFree.addEventListener('click',function(){
+    deliveryCost.innerText ="0";
+    updateTotal();
+ });
+deliveryTwo.addEventListener('click',function(){
+    deliveryCost.innerText ="20";
+    updateTotal();
+ });
+//finalPricePromo fuction area
+function finalPricePromo() {
+    const promoField = document.getElementById('promo-input');
+    const priceFinal = document.getElementById('total-price-final');
+    const totalPrice = document.getElementById('total-price');
+    const discount = parseFloat(totalPrice.innerHTML) / 100 * 20;
+    if (promoField.value == 'stevekaku') {
+      const totalPriceFinalPromo = parseFloat(totalPrice.innerHTML) - discount;
+      priceFinal.innerHTML = totalPriceFinalPromo;
+    }
+    else if (promoField.value = '') {
+        
+    }
+    else {
+        alert("Invalid Promo Code");
+    }
+    promoField.value = '';
+};
 
-});
-document.getElementById('storage-256').addEventListener('click',function(){
-     const storageButton = document.getElementById('storage-cost');
-     storageButton.innerText = 0;
 
-});
-document.getElementById('storage-512').addEventListener('click',function(){
-     const storageButton = document.getElementById('storage-cost');
-     storageButton.innerText = 100;
 
-});
-document.getElementById('storage-1tb').addEventListener('click',function(){
-     const storageButton = document.getElementById('storage-cost');
-     storageButton.innerText = 180;
-
+// Promo Code Apply
+document.getElementById('promo-apply').addEventListener('click', function () {
+finalPricePromo();
 });
